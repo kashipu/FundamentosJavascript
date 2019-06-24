a0c91e2fff57e20da3d79aeb8a8f350f1c07e4f1
kashipu <graficowm@gmail.com>
2019-06-24 11:17:59 -0500

    Clase 3 y 4

---
+++ clase3.js
@@ -0,0 +1,17 @@
+var edad = 26
+
+/* edad = edad + 1;  */
+
+// Incremento 1
+edad +=1
+// Negativos
+edad -= 1
+
+var precioVino = 200.3
+
+var total = Math.round(precioVino * 100 * 3) / 100
+
+var totalStr = total.toFixed(3)
+
+var total2 = parseFloat(totalStr);
+
---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'William',
+    edad = 26
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Carlos', 26)
+imprimirEdad('Jhonathan', 17)
